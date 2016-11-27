22695e34cc40d9b695e00b644f889600d26be90f
Alexander Cherkashin <Alexander Cherkashin>
2016-11-28 01:34:05 +0300

    refactoring

--- leaflet-toolbar/leaflet-toolbar.js
+++ leaflet-toolbar/leaflet-toolbar.js
@@ -8,19 +8,7 @@ function setOrientation(domItem, orientation) {
     }
 }
 
-function createToolbarItem(baseOptions, itemOptions, container) {
-    const classes = `${baseOptions.template.classes} ${itemOptions.classes}`;
-    const item = L.DomUtil.create(baseOptions.template.tag, classes, container);
-    item.title = itemOptions.title;
-    item.style.margin = '0 auto';
-
-    setOrientation(item, baseOptions.orientation);
-
-    return item;
-}
-
-
-L.FunctionButtons = L.Control.extend({
+L.toolbar = L.Control.extend({
     includes: L.Mixin.Events,
 
     initialize: function (buttons, options) {
@@ -46,30 +34,10 @@ L.FunctionButtons = L.Control.extend({
         for (let i = 0; i < this._groupsButton.length; i++) {
             const buttons = this._groupsButton[i];
             const groupContainer = L.DomUtil.create('div', this._options.groupTemplate.classes, container);
+
             for (let j = 0; j < buttons.length; j++) {
                 let button = buttons[j];
-                let domItem = createToolbarItem(this._options, button, groupContainer);
-
-                domItem._groupsButtonIndex = i;
-                domItem._buttonIndex = j; // todo: remove?
-
-                if (button.title) {
-                    domItem.title = button.title;
-                }
-
-                button.domItem = domItem;
-
-                var stop = L.DomEvent.stopPropagation;
-                L.DomEvent
-                    .on(domItem, 'click', stop)
-                    .on(domItem, 'mousedown', stop)
-                    .on(domItem, 'dblclick', stop);
-
-                if (!button.href) {
-                    L.DomEvent
-                        .on(domItem, 'click', L.DomEvent.preventDefault)
-                        .on(domItem, 'click', this.clicked, this);
-                }
+                this._createToolbarItem(this._options, button, groupContainer, i, j);
             }
         }
 
@@ -93,9 +61,54 @@ L.FunctionButtons = L.Control.extend({
                 this.fire('clicked', { idx: domItem._buttonIndex });
             }
         }
+    },
+
+    /**
+     * @param  {} baseOptions - common options for all toolbar
+     * @param  {} itemOptions - options for item button
+     * @param  {} container - container, where will be placed toolbar item
+     * @param  {Number} groupIndex - index of group 
+     * @param  {Number} buttonIndex - index of index
+     */
+    _createToolbarItem: function (baseOptions, itemOptions, container, groupIndex, buttonIndex) {
+        let domItem = this._createToolbarDOMItem(baseOptions, itemOptions, container);
+
+        domItem._groupsButtonIndex = groupIndex;
+        domItem._buttonIndex = buttonIndex;
+
+        if (itemOptions.title) {
+            domItem.title = itemOptions.title;
+        }
+
+        itemOptions.domItem = domItem;
+
+        //Prevents the event from bubbling up the DOM tree, preventing any parent handlers from being notified of the even
+        var stop = L.DomEvent.stopPropagation;
+        L.DomEvent
+            .on(domItem, 'mousedown', stop)
+            .on(domItem, 'dblclick', stop)
+            .on(domItem, 'click', stop)
+            .on(domItem, 'click', L.DomEvent.preventDefault) //If this method is called, the default action of the event will not be triggered
+            .on(domItem, 'click', this.clicked, this);
+    },
+
+    /**
+     * @param  {} baseOptions - common options for all toolbar
+     * @param  {} itemOptions - options for item button
+     * @param  {} container - container, where will be placed toolbar item
+     */
+    _createToolbarDOMItem: function (baseOptions, itemOptions, container) {
+        const classes = `${baseOptions.template.classes} ${itemOptions.classes}`;
+        const item = L.DomUtil.create(baseOptions.template.tag, classes, container);
+        item.title = itemOptions.title;
+        item.style.margin = '0 auto';
+
+        setOrientation(item, baseOptions.orientation);
+
+        return item;
     }
 });
 
 L.Toolbar = function (buttons, options) {
-    return new L.FunctionButtons(buttons, options);
+    return new L.toolbar(buttons, options);
 };
