e77e1524992dfb4fee32d74c855c795ebcee7875
Alexander Cherkashin <Alexander Cherkashin>
2016-11-28 00:28:36 +0300

    remove unnecessary methods

--- leaflet-toolbar/leaflet-toolbar.js
+++ leaflet-toolbar/leaflet-toolbar.js
@@ -1,7 +1,3 @@
-/*
- * A leaflet button with icon or text and click listener.
- * http://mapbbcode.org/leaflet.html
- */
 function setOrientation(domItem, orientation) {
     if (orientation === 'row') {
         domItem.style.display = 'inline-block';
@@ -47,24 +43,12 @@ L.FunctionButtons = L.Control.extend({
                 domItem = createToolbarItem(this._options, button, container);
 
             domItem._buttonIndex = i; // todo: remove?
-            domItem.href = button.href || '#';
-            if (button.href) {
-                domItem.target = 'funcbtn';
-            }
-
-            domItem.style.padding = '0 4px';
-            domItem.style.width = 'auto';
-            domItem.style.minWidth = '20px';
-            if (button.bgColor) {
-                domItem.style.backgroundColor = button.bgColor;
-            }
 
             if (button.title) {
                 domItem.title = button.title;
             }
 
             button.domItem = domItem;
-            // this._updateContent(i);
 
             var stop = L.DomEvent.stopPropagation;
             L.DomEvent
@@ -80,85 +64,6 @@ L.FunctionButtons = L.Control.extend({
         }
 
         return container;
-    },
-
-    _updateContent: function (n) {
-        if (n >= this._buttons.length)
-            return;
-        var button = this._buttons[n],
-            domItem = button.domItem,
-            content = button.content;
-        if (!domItem)
-            return;
-        if (content === undefined || content === false || content === null || content === '')
-            domItem.innerHTML = button.alt || '&nbsp;';
-        else if (typeof content === 'string') {
-            var ext = content.length < 4 ? '' : content.substring(content.length - 4),
-                isData = content.substring(0, 11) === 'data:image/';
-            if (ext === '.png' || ext === '.gif' || ext === '.jpg' || isData) {
-                domItem.style.width = '' + (button.imageSize || 26) + 'px';
-                domItem.style.height = '' + (button.imageSize || 26) + 'px';
-                domItem.style.padding = '0';
-                domItem.style.backgroundImage = 'url(' + content + ')';
-                domItem.style.backgroundRepeat = 'no-repeat';
-                domItem.style.backgroundPosition = button.bgPos ? (-button.bgPos[0]) + 'px ' + (-button.bgPos[1]) + 'px' : '0px 0px';
-            } else
-                domItem.innerHTML = content;
-        } else {
-            while (domItem.firstChild)
-                domItem.removeChild(domItem.firstChild);
-            domItem.appendChild(content);
-        }
-    },
-
-    setContent: function (n, content) {
-        if (content === undefined) {
-            content = n;
-            n = 0;
-        }
-        if (n < this._buttons.length) {
-            this._buttons[n].content = content;
-            this._updateContent(n);
-        }
-    },
-
-    setTitle: function (n, title) {
-        if (title === undefined) {
-            title = n;
-            n = 0;
-        }
-        if (n < this._buttons.length) {
-            var button = this._buttons[n];
-            button.title = title;
-            if (button.domItem)
-                button.domItem.title = title;
-        }
-    },
-
-    setBgPos: function (n, bgPos) {
-        if (bgPos === undefined) {
-            bgPos = n;
-            n = 0;
-        }
-        if (n < this._buttons.length) {
-            var button = this._buttons[n];
-            button.bgPos = bgPos;
-            if (button.domItem)
-                button.domItem.style.backgroundPosition = bgPos ? (-bgPos[0]) + 'px ' + (-bgPos[1]) + 'px' : '0px 0px';
-        }
-    },
-
-    setHref: function (n, href) {
-        if (href === undefined) {
-            href = n;
-            n = 0;
-        }
-        if (n < this._buttons.length) {
-            var button = this._buttons[n];
-            button.href = href;
-            if (button.domItem)
-                button.domItem.href = href;
-        }
     },
 
     clicked: function (e) {
